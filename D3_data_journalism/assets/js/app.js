d8a6c1d31fa65a4f551c607591323c91055ab455
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-05-06 22:54:27 +0800

    first stage

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,16 @@
 // @TODO: YOUR CODE HERE!
+var cdata = ("assets/data/data.csv")
+
+function init() {
+    var selData = d3.select("#scatter");
+    d3.csv(cdata).then(function(data) {
+    // Visualize the data
+    console.log(data);
+
+  });
+};
+
+
+
+
+init();
