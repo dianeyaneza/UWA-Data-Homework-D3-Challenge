87a57f9fc890675068a265c20a35cb87b9e81c78
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-05-08 16:13:13 +0800

    prepared chart space

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,9 +6,25 @@ function init() {
     d3.csv(cdata).then(function(data) {
     // Visualize the data
     console.log(data);
-
   });
 };
+
+var width = parseInt(d3.select("#scatter").style("width"));
+var height = width - width / 3.9;
+var labelArea = 110;
+var margin = 20;
+
+var tPadBot = 40;
+var tPadLeft = 40;
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "chart");
+  console.log(width);
+  console.log(height);
 
 
 
